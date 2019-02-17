aff0a8c20c85aa7db9288f3bc74ea8d6d7646766
Oleg Sitnikov <o.sitnikov@pr-solution.ru>
2019-02-17 17:13:25 +0300

    init

--- kotlin/shrine/app/src/main/java/com/google/codelabs/mdc/kotlin/shrine/LoginFragment.kt
+++ kotlin/shrine/app/src/main/java/com/google/codelabs/mdc/kotlin/shrine/LoginFragment.kt
@@ -2,9 +2,12 @@ package com.google.codelabs.mdc.kotlin.shrine
 
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.text.Editable
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import kotlinx.android.synthetic.main.shr_login_fragment.*
+import kotlinx.android.synthetic.main.shr_login_fragment.view.*
 
 /**
  * Fragment representing the login screen for Shrine.
@@ -17,8 +20,45 @@ class LoginFragment : Fragment() {
 
         // Snippet from "Navigate to the next Fragment" section goes here.
 
-        return inflater.inflate(R.layout.shr_login_fragment, container, false)
+        val view = inflater.inflate(R.layout.shr_login_fragment, container, false)
+        // Set an error if the password is less than 8 characters.
+        view.next_button.setOnClickListener {
+            if (!isPasswordValid(password_edit_text.text!!)) {
+                password_text_input.error = getString(R.string.shr_error_password)
+            } else {
+                // Clear the error.
+                password_text_input.error = null
+            }
+        }
+
+        // Clear the error once more than 8 characters are typed.
+        view.password_edit_text.setOnKeyListener { _, _, _ ->
+            if (isPasswordValid(password_edit_text.text!!)) {
+                // Clear the error.
+                password_text_input.error = null
+            }
+            false
+        }
+
+        // Set an error if the password is less than 8 characters.
+        view.next_button.setOnClickListener {
+            if (!isPasswordValid(password_edit_text.text!!)) {
+                password_text_input.error = getString(R.string.shr_error_password)
+            } else {
+                // Clear the error.
+                password_text_input.error = null
+                // Navigate to the next Fragment.
+                (activity as NavigationHost).navigateTo(ProductGridFragment(), false)
+            }
+        }
+
+
+        return view
     }
 
     // "isPasswordValid" from "Navigate to the next Fragment" section method goes here
+}
+
+private fun isPasswordValid(text: Editable?): Boolean {
+    return text != null && text.length >= 8
 }
